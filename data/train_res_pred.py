2c8465f3c1068b82761841f0eb45e4086ffc5cd2
Hwanmoo Yong <hwanmooy@gmail.com>
2018-11-09 09:29:40 +0900

    f

--- data/train_res_pred.py
+++ data/train_res_pred.py
@@ -100,11 +100,11 @@ model.compile(loss='mean_squared_error',
               optimizer='adam',
               metrics=['accuracy'])
 model_json = model.to_json()
-with open("model.json", "w") as json_file : 
-    json_file.write(model_json)
+# with open("model.json", "w") as json_file : 
+#     json_file.write(model_json)
 
 
-exit(-1)
+# exit(-1)
 if data_augmentation:
     print('Using data augmentation.')
     # X_train = seq.augment_images(X_train)
